adb71f23d7b5d677e9f203f7f0195e74140d34c8
zengjia121 <635600841@qq.com>
2024-07-08 11:31:30 +0800

    chore: 更新页脚评论组件

--- docs/.vitepress/theme/components/FooterComment.vue
+++ docs/.vitepress/theme/components/FooterComment.vue
@@ -4,37 +4,35 @@
   </div>
 </template>
 <script setup lang='ts'>
-import { onMounted } from "vue"
-
+import { onMounted, ref } from "vue"
 import { useData } from "vitepress"
 import { usePageId } from "../composables"
 const { theme } = useData()
 const pageId = usePageId()
-if (typeof window !== "undefined") {
-  import("valine").then((module) => {
-    Valine.value = module.default
-  })
-}
 const { visitor } = theme.value
 onMounted(async () => {
-  if (Valine.value) {
-    new Valine.value({
-      el: "#vcomments",
-      appId: "fCdfcJuzbLAvebzaHbh0Atfx-gzGzoHsz",
-      appKey: "NIzvVdueG6ekaX3mFpq3Y8r8",
-      placeholder: "说点什么吧...",
-      avatar: "hide",
-      path: visitor.badgeId ? `${visitor.badgeId}.${pageId}` : pageId,
-      visitor: true,
-      recordIP: true,
-    })
+  if (typeof window === "undefined") {
+    return
   }
+  const Valine = await (await import("valine")).default
+  new Valine({
+    el: "#vcomments",
+    appId: "fCdfcJuzbLAvebzaHbh0Atfx-gzGzoHsz",
+    appKey: "NIzvVdueG6ekaX3mFpq3Y8r8",
+    placeholder: "说点什么吧...",
+    avatar: "hide",
+    path: visitor.badgeId ? `${visitor.badgeId}.${pageId}` : pageId,
+    visitor: true,
+    recordIP: true,
+    meta: ["nick"],
+  })
 })
 </script>
 
 
 <style lang='scss' scoped>
 #vcomments {
-  margin-top: 15px;
+  margin-top: 30px;
+  border-radius: 2%;
 }
 </style>
